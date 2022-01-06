88ff16317799c370467cc2b16c7bc28dbddcbf3d
Mike Wong <michael.d.wong362@gmail.com>
2022-01-05 20:29:01 -0800

    Enable GPU

--- detect.py
+++ detect.py
@@ -4,10 +4,12 @@ from utils import *
 from PIL import Image, ImageDraw, ImageFont
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 # Load model checkpoint
 checkpoint = 'checkpoint_ssd300.pth.tar'
-checkpoint = torch.load(checkpoint,  map_location=torch.device('cpu'))
+if not torch.cuda.is_available():
+    checkpoint = torch.load(checkpoint,  map_location=torch.device('cpu'))
+else:
+    checkpoint = torch.load(checkpoint)
 start_epoch = checkpoint['epoch'] + 1
 print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
 model = checkpoint['model']
@@ -59,6 +61,8 @@ def detect(original_image, min_score, max_overlap, top_k, pid, suppress=None):
 
     # If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
     if det_labels == ['background']:
+        with open(f'{pid}-predictions.csv', 'w') as f:
+            f.write(f'left,top,right,bottom,class,confidence\n')
         # Just return original image
         return original_image
 
@@ -119,4 +123,4 @@ if __name__ == '__main__':
     args = ap.parse_args()
     original_image = Image.open(args.input, mode='r')
     original_image = original_image.convert('RGB')
-    detect(original_image, min_score=0.09, max_overlap=0.5, top_k=200, pid=args.pid)
+    detect(original_image, min_score=0.4, max_overlap=0.5, top_k=200, pid=args.pid)
