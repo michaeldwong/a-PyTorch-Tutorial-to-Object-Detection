bff876f6ce5fa18391456b53f648bac72869ad32
Mike Wong <michael.d.wong362@gmail.com>
2022-01-05 19:30:19 -0800

    Output csv files

--- detect.py
+++ detect.py
@@ -1,3 +1,4 @@
+import argparse
 from torchvision import transforms
 from utils import *
 from PIL import Image, ImageDraw, ImageFont
@@ -20,7 +21,7 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
 
-def detect(original_image, min_score, max_overlap, top_k, suppress=None):
+def detect(original_image, min_score, max_overlap, top_k, pid, suppress=None):
     """
     Detect objects in an image with a trained SSD300, and visualize the results.
 
@@ -65,41 +66,57 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
     annotated_image = original_image
     draw = ImageDraw.Draw(annotated_image)
     font = ImageFont.load_default()
-    #font = ImageFont.truetype("./calibril.ttf", 15)
 
     # Suppress specific classes, if needed
-    for i in range(det_boxes.size(0)):
-        if suppress is not None:
-            if det_labels[i] in suppress:
-                continue
 
-        # Boxes
-        box_location = det_boxes[i].tolist()
-        draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
-        draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
-            det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
-        # draw.rectangle(xy=[l + 2. for l in box_location], outline=label_color_map[
-        #     det_labels[i]])  # a third rectangle at an offset of 1 pixel to increase line thickness
-        # draw.rectangle(xy=[l + 3. for l in box_location], outline=label_color_map[
-        #     det_labels[i]])  # a fourth rectangle at an offset of 1 pixel to increase line thickness
+    with open(f'{pid}-predictions.csv', 'w') as f:
+        f.write(f'left,top,right,bottom,class,confidence\n')
+        for i in range(det_boxes.size(0)):
+            if suppress is not None:
+                if det_labels[i] in suppress:
+                    continue
+            print(det_labels[i])
+            print(det_scores[0][i].item())
+            # Boxes
+            box_location = det_boxes[i].tolist()
+            for j in range(len(box_location)):
+                box_location[j] = int(max(box_location[j], 0))
+            f.write(f'{int(box_location[0])},{int(box_location[1])},{int(box_location[2])},{int(box_location[3])},{det_labels[i]},{det_scores[0][i].item()}\n')
 
-        # Text
-        text_size = font.getsize(det_labels[i].upper())
-        text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
-        textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
-                            box_location[1]]
-        draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
-        draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
-                  font=font)
-    del draw
+            draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
+            draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
+                det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
 
-    print('Type of annotated : ', type(annotated_image))
+            # Text
+            text_size = font.getsize(det_labels[i].upper())
+            text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
+            textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
+                                box_location[1]]
+            draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
+            draw.text(xy=text_location, text=f'{det_labels[i]} ({str(det_scores[0][i].item())})', fill='white',
+                      font=font)
+#    del draw
     annotated_image.save('predictions.png')
     return annotated_image
 
 
 if __name__ == '__main__':
-    img_path = 'dahua2-z2.png'
-    original_image = Image.open(img_path, mode='r')
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "--input",
+        help="Input image"
+#        nargs="+",
+        #help="A list of space separated input images; "
+#        "or a single glob pattern such as 'directory/*.jpg'",
+    )
+
+    ap.add_argument(
+        "--pid",
+        help="PID for controllable camera experiments",
+        default="0"
+    )
+
+    args = ap.parse_args()
+    original_image = Image.open(args.input, mode='r')
     original_image = original_image.convert('RGB')
-    detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200)
+    detect(original_image, min_score=0.09, max_overlap=0.5, top_k=200, pid=args.pid)
